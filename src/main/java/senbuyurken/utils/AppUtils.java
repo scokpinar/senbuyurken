9308f528ef964e519e4318bcb4f4ebb24969686e
samet <sametcokpinar@gmail.com>
2016-10-28 17:10:17 +0300

    App ID Change

--- src/main/java/senbuyurken/utils/AppUtils.java
+++ src/main/java/senbuyurken/utils/AppUtils.java
@@ -11,9 +11,9 @@ public class AppUtils {
 
     public static final String GOOGLE_CLIENT_ID = "345121036471-p2rragjceuga9g0vrf04e8ml7komc07m.apps.googleusercontent.com";
     //Local
-    //public static final String GOOGLE_APP_ID = "345121036471-er4m3a2bn1f2itqio1c8ije49oahfpu3.apps.googleusercontent.com";
+    public static final String GOOGLE_APP_ID = "345121036471-er4m3a2bn1f2itqio1c8ije49oahfpu3.apps.googleusercontent.com";
     //Beta
-    public static final String GOOGLE_APP_ID = "345121036471-vl8dv1c20chsvlh2jkhboqpqlsfc12u5.apps.googleusercontent.com";
+    //public static final String GOOGLE_APP_ID = "345121036471-vl8dv1c20chsvlh2jkhboqpqlsfc12u5.apps.googleusercontent.com";
 
 
     public static Boolean tokenValidator(String token) {
